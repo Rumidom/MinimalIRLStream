593e639040096a461782013ba0b7226d8700dd37
rumidom <jr.robertocv@gmail.com>
2025-05-10 11:33:40 -0300

    fix: preserving state upon page change

--- minimalstreammonitor/lib/ui/components.dart
+++ minimalstreammonitor/lib/ui/components.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'style.dart';
+import 'dart:async';
 
+Timer makePeriodicTimer(
+  Duration duration,
+  void Function(Timer timer) callback, {
+  bool fireNow = false,
+}) {
+  var timer = Timer.periodic(duration, callback);
+  if (fireNow) {
+    callback(timer);
+  }
+  return timer;
+}
 
 Container wideButton(text,callback) {
   return Container(
@@ -16,4 +29,16 @@ Container wideButton(text,callback) {
       ),
       onPressed: callback,
       child: Text(text,style: ThemeText.buttontextstyle)));
+}
+
+void toastmessage(String msg){
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.CENTER,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.grey,
+    textColor: Colors.black,
+    fontSize: 16.0
+  );
 }
