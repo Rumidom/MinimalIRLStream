35f5f6353df70bd1b494e1bdda4c310cbde530ad
rumidom <jr.robertocv@gmail.com>
2025-05-27 19:43:54 -0300

    feat: bug fixes and adding value bubbles

--- minimalstreamserver/server.py
+++ minimalstreamserver/server.py
@@ -3,8 +3,9 @@ from io import BytesIO
 import time
 import utils.redisfunc as rds
 import utils.UI as ui
+from datetime import datetime
+import os
 
-DataDict = {'steps':rds.getData(rds.r,'steps'),'distance':rds.getData(rds.r,'distance'),'heartrates':rds.getData(rds.r,'heartrates')}
 print('Starting')
 
 layout = [
@@ -13,6 +14,36 @@ layout = [
 
 window = sg.Window('MIRL Server', layout, keep_on_top=True)
 last_Img = None
+save_images = True
+clear_day = True
+StartingDateTime = datetime.now()
+foldername = StartingDateTime.strftime("SessionImages_%Y-%m-%d_%H_%M_%S")
+
+def saveImage(im):
+    current_datetime = datetime.datetime.now()
+    timestamp = current_datetime.timestamp()
+    if not os.path.exists(foldername):
+        os.makedirs(foldername)
+    im.save(foldername+"/"+str(timestamp)+".png")
+
+def checkDict(di):
+    for key in di:
+        if len(di[key]) == 0:
+            return False
+    return True
+    
+print('Starting')
+print('Time: ' + StartingDateTime.strftime("%Y-%m-%d %H:%M:%S"))
+if rds.flushServer():
+    print('Server flushed.')
+else:
+    print('Error flushing server')
+print('Seting BBImg')
+rds.setbbimgKey()
+
+
+DataDict = {'steps':rds.getData(rds.r,'steps'),'distance':rds.getData(rds.r,'distance'),'heartrates':rds.getData(rds.r,'heartrates')}
+print("Data: ", DataDict)
 while True:
     message = rds.p.get_message()
     event, values = window.read(timeout=10)
@@ -26,16 +57,20 @@ while True:
         if message['data'] == 'lpush':
             dataUpdate = rds.getLastDataUpdate(rds.r,key)
             print(key,":",dataUpdate)
-            rds.addData(DataDict[key],dataUpdate)
+            if dataUpdate != None:
+                rds.addData(DataDict[key],dataUpdate)
         elif message['data'] == 'hset':
             if key == 'imMetaDataJson':
                 imJSON = rds.getImgJson()
                 print('URL: ',imJSON['url'],'Timestamp: ',imJSON['timestamp']) 
                 last_Img = ui.downloadImgbb(imJSON,saveToStreamLogs = True,finalimgsize=(1280,720))
-                
-        frame = ui.GenerateFrame(heartRateData=DataDict['heartrates'],stepsData=DataDict['steps'],photo=last_Img)
-        frame.thumbnail((1024, 512))
-        window['Image'].update(data=ui.image_to_data(frame), size=(1024,512))
+        
+        if checkDict(DataDict):
+            frame = ui.GenerateFrame(distanceData = DataDict['distance'],heartRateData=DataDict['heartrates'],stepsData=DataDict['steps'],photo=last_Img)
+            frame.thumbnail((1024, 512))
+            saveImage(frame)
+            window['Image'].update(data=ui.image_to_data(frame), size=(1024,512))
+
     else:
         time.sleep(0.01)
 
